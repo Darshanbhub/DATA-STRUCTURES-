da42ff3f297a3cc9a714eb2207d0d516c2d2a547
Darshanbhub <92705846+Darshanbhub@users.noreply.github.com>
2021-12-12 21:27:12 +0530

    Update CircularQueue.c

--- CircularQueue.c
+++ CircularQueue.c
@@ -1,100 +1,84 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#define SIZE 5
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+int rear=-1;
+int front =-1;
+int elements[SIZE];
 
-int items[SIZE];
-int front = -1, rear=-1;
-
-bool isFull(){
-    printf("%d", front);
-    printf("%d", rear);
-    if((front==(rear +1) % SIZE)||(front ==0 && rear == SIZE -1))
-    return true;
-    else false;
-}
-
-int isEmpty(){
-    if(front==-1)
-    return 1;
-}
-
-void enQueue(int element){
-    if(isFull())
-    printf("\nQueue is full!!\n");
-    else{
-        if(front==-1)
-        front =0;
-        rear=0;
-        rear = (rear +1)%SIZE;
-        items[rear]=element;
-        printf("\nInserted --> %d", element);
-    }
-}
-
-int deQueue()
+void insert()
 {
-    int element;
-    if(isEmpty()){
-        printf("\nQueue is empty!!\n");
-        return(-1);
+    if((rear == SIZE-1)||(rear == front -1))
+    printf("ThE QUEUE is full\n");
+    else
+    {
+        if(front==-1&&rear==-1)
+        {
+        front = 0;
+        rear =0;
+        }
+        
+        
+            printf("Enter a number to be inserted: \n");
+            int item;
+            scanf("%d", &item);
+            elements[rear]= item;
+            rear = (rear +1)%SIZE;
+            printf("Number inserted is --> %d \n ", item);
+            
         
     }
-    else{
-        element = items[front];
-        if(front==rear){
-            front = -1;
-            rear = -1;
-        }
-        else{
-            front = (front +1)%SIZE;
-        }
-        printf("\nDeleted element --> %d\n", element);
-        return (element);
+}
+
+void display()
+{
+    if((front==-1&&rear==-1))
+    printf("The queue is empty\n");
+    else
+    {
+        printf("ELEMENTS: \n");
+        int i;
+       
+        for( i=front; i!=rear; i =(i+1)%SIZE)
+        printf("%d \n",elements[i]);
     }
 }
 
-void display(){
-    int i;
-    if (isEmpty())
-    printf("\n Empty queue\n");
-    else{
-        printf("\nFront --> %d", front);
-        printf("\nItems -->");
-        for(i=front; i!=rear; i=(i+1)%SIZE){
-            printf("%d", items[1]);
-            printf("\nRear --> %d\n", rear);
-        }
+void delete()
+{
+    if((front==-1&&rear==-1))
+    printf("The queue is empty\n");
+    else
+    {
+        printf("Deleted element --> %d", elements[front]);
+        front = (front + 1)%SIZE;
+        
     }
 }
+
 int main()
 {
-    int choice;
+    int input;
     while(1)
     {
-        printf("1.ENQUEUE   2.DEQUEUE   3.DSIPLAY   4.EXIT\n");
-        printf("Enter your choice:");
-        scanf("%d", &choice);
-        switch(choice)
-        {
-            case 1:
-            enQueue(choice);
-            break;
-            
-            case 2:
-            deQueue();
-            break;
-            
-            case 3:
-            display();
-            break;
-            
-            case 4:
-            exit(0);
-            
-            
-            default:printf("Invalid choice\n");
-        }
+    printf("Enter your choice \n");
+    printf("1.Insert\n 2.Delete\n 3.Display\n 4.Exit\n");
+    scanf("%d", &input);
+    switch(input)
+    {
+        case 1:
+        insert();
+        break;
+        case 2:
+        delete();
+        break;
+        case 3:
+        display();
+        break;
+        case 4:
+        exit(0);
+        default:printf("Invalid entry try again \n");
+    }
     }
     return 0;
 }
+
