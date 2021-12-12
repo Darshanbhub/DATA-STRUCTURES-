eb5a3526ec1cf9eabfe1a93cb850c1ba6c20066f
Darshanbhub <92705846+Darshanbhub@users.noreply.github.com>
2021-12-12 21:25:36 +0530

    Update Linear Queue.c

--- Linear Queue.c
+++ Linear Queue.c
@@ -1,39 +1,97 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 50
-int queue_array[50];
-int rear =-1;
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 5
 int front = -1;
-
-void display()
-{
-    int i;
-    if (front == -1)
-    printf("Queue is empty\n");
-    else
-    {
-        printf("Queue is \n");
-        for(i= front; i <=rear; i++)
-        printf("%d", queue_array[i]);
-        printf("\n");
-    }
-}
-
+int rear = -1;
+int stack[SIZE];
 
 void insert()
 {
-    int add_item;
-    if(rear ==MAX -1)
-    printf("Queue Overflow\n");
+    if(rear == SIZE -1)
+    printf("Stack overflow! (Stack is full)\n");
     else
     {
-        if(front==-1)
-        front = 0;
-        printf("Insert the element in queue :");
-        scanf("%d", &add_item);
-        rear = rear +1;
-        queue_array[rear] = add_item;
+        if(front==-1 && rear==-1)
+        {
+            front = 0;
+            rear = 0;
+            
+        }
+        else
+        {
+            int ele;
+            printf("Enter the number to be inserted : \n");
+            scanf("%d", &ele);
+            stack[rear++]= ele;
+            printf("Elemen inserted %d\n", ele);
+            }
     }
+}
+
+void delete()
+{
+    if((front==-1&&rear==-1)||(front == rear  ))
+    printf("Stack underflow! (Stack is empty)");
+    else
+    {
+        printf("Deleted element is --> %d", stack[front++]);
+        
+    }
+}
+
+void display()
+{
+    if((front==-1&&rear==-1)||(front == rear ))
+    printf("Stack underflow! (Stack is empty)");
+    else
+    {
+        printf("Queue elements are -->");
+        for(int i = front; i!=rear; i++)
+        printf("%d ", stack[i]);
+    }
+    
+}
+
+int main()
+{
+    int choice;
+    while(1)
+    {
+        printf("1.insert  2.delete   3.display 4.EXIT\n");
+        printf("Enter your choice:");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+            insert();
+            break;
+            
+            case 2:
+            delete();
+            break;
+            
+            case 3:
+            display();
+            break;
+            
+            case 4:
+            exit(0);
+            
+            
+            default:printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
 }
 
 void delete()
